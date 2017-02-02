bc637f04523e08666da046fe5b27ebcaa4d65e24
cremond <cremond@pechel.weisshorn.calais.fr>
2017-02-02 13:19:05 +0100

    Add basic files for the netbeans project SHOT

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: cremond
+ *
+ * Created on 2 février 2017, 13:13
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
